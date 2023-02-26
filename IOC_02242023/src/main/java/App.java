df5981e2439c3854a3370334748ab8b89c14c05d
Savyata Gomden <savyatag@amdocs.com>
2023-02-26 11:53:25 -0600

    First Commit

---
+++ IOC_02242023/src/main/java/App.java
@@ -0,0 +1,19 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.backendpractice.pojo.Employee;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("appConfig.xml");
+		Employee e1 = (Employee)appContext.getBean("e1");
+		
+		System.out.println(e1.getFirstName()+" "+e1.getLastName()+", "+e1.getAddress().getCity()+","
+		+e1.getAddress().getState()+", "+e1.getAddress().getCountry());
+
+	}
+
+}
